fc9f9a53bbff061468a913653c0665917d2ae562
yaseld55 <132209453+yaseld55@users.noreply.github.com>
2024-02-04 03:51:02 +0800

    updated

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -184,7 +184,7 @@ export const forgetPass = async (req, res) => {
       subject: 'Password Reset',
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
       Please click on the following link, or paste this into your browser to complete the process:\n\n
-      http://${req.headers.host}/reset-password/${resetToken}\n\n
+      https://precious-crepe-8660ad.netlify.app/reset-password/${resetToken}\n\n
       If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
